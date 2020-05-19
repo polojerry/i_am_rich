20e1f6eec5a59bca6f97f7262a18f9e1fc492e0a
polojerry <polojerry4@gmail.com>
2020-05-19 15:56:07 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//The main() is the entry/starting point for every flutter application
+void main() {
+  runApp(MaterialApp(
+    home: Center(
+      child: Text("Hello World"),
+    ),
+  ));
+}
