c9512bbb3929a4da1b973cef46771db68223ce29
polojerry <polojerry4@gmail.com>
2020-05-19 16:35:24 +0300

    Added a Scaffold, AppBar, Image:-Loaded from the internet

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 //The main() is the entry/starting point for every flutter application
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello World"),
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text("I Am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage('https://www.w3schools.com/css/img_lights.jpg'),
+        ),
+      ),
     ),
   ));
 }
